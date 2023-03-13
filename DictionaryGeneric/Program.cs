c97897b74bded410156d15acb988880c7a2bb138
My Name <kiykobogdan@gmail.com>
2023-03-13 12:23:49 +0200

    Add project files.

---
+++ DictionaryGeneric/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary obj1 = new Dictionary();
+            obj1.AddWord("cat", "Кот/кошка");
+            obj1.Print();
+            Console.WriteLine(obj1.GetEnglishTranslate("cat"));
+            Console.WriteLine(obj1.GetRussianTranslate("Кот/кошка"));
+            Console.ReadKey();
+        }
+    }
+}
